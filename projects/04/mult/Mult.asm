73c249f6bdf5d10bb3fda917ed4ca2d95ca179e2
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2023-05-05 00:23:48 +0900

    Pass Mult in project 04

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,34 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// アルゴリズム → R0をR1回分足し算する
+// 高級言語の場合
+// R2=0
+// i=0
+// while i < R1:
+//   R2 += R0
+//   i++
+  
+    @R2
+    M=0
+    @i
+    M=1
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JGT
+    @R0
+    D=M
+    @R2
+    M=M+D
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP   // Goto END (infinite loop)
